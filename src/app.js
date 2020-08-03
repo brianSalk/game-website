910f5e309c2c588dabe83000940b88fe7a9b2f0e
unknown <brian.salkas@gmail.com>
2020-08-02 20:26:34 -0500

    init commit

---
+++ src/app.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const chalk = require('chalk')
+const hbs = require('hbs')
+const {tabs} = require('./arrays/tabs.js')
+hbs.registerPartials('handlebars/partials')
+const port = process.env.PORT | 3000
+const app = express()
+app.use(express.static('public'))
+app.set('view engine', 'hbs')
+app.set('views', 'handlebars/views')
+
+app.get('/', (req,res) => {
+    res.render('index.hbs', {tabs})
+})
+
+app.listen(port, () => console.log(chalk.greenBright(`listening on port ${port}`)))
---
+++ src/arrays/tabs.js
@@ -0,0 +1,10 @@
+const tabs = [
+    {dropdown: 'games', dropdown_items: [{href: '#', label: 'play'},{href: '#', label: 'download'},{href: '#', label: 'search'}]},
+    {dropdown: 'users', dropdown_items: [{href: '#', label: 'log-in'},{href: '#', label: 'signup'}]},
+    {button: 'about', href:'#'},
+    {dropdown: 'other', dropdown_items: [{href: '#', label: 'item1'},{href: '#', label: 'item2'},{href: '#', label: 'item3'},{href: '#', label: 'item4'}]},
+]
+
+module.exports = {
+    tabs
+}
